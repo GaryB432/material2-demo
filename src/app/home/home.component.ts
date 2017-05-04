3f145926f2d8b0d80e5752131f2249a9edb51433
Gary Bortosky <garybort@gmail.com>
2017-05-04 15:34:35 -0500

    add some list items

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,12 @@ export class HomeComponent implements OnInit {
   public position: TooltipPosition = "below";
   public message: string = "This is a tooltip!";
 
+  public foods: any[] = [
+    { name: "Pizza", rating: "Excellent" },
+    { name: "Burritos", rating: "Great" },
+    { name: "French fries", rating: "Pretty good" },
+  ];
+
   public ngOnInit(): void {
     console.log("Hello Home");
   }
